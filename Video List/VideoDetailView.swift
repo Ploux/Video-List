05a0b53d11cc03ede7e9727b8466ea49eccf74d7
Ploux <peter@humo.golf>
2024-06-11 14:04:49 -0500

    update detail view

--- Video List/VideoDetailView.swift
+++ Video List/VideoDetailView.swift
@@ -10,7 +10,30 @@ import SwiftUI
 struct VideoDetailView: View {
     var video: Video
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(video.imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 150)
+                .cornerRadius(12)
+            Text(video.title)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .lineLimit(2)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            HStack {
+                Label("\(video.viewCount)", systemImage: "eye.fill")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            
+                Text(video.uploadDate)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+
+        }
     }
 }
 
