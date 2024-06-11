5d86d701bbc696b5837ca57b7aa332ad94309543
Ploux <peter@humo.golf>
2024-06-11 12:41:10 -0500

    Initial Commit

---
+++ Video List/Video_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Video_ListApp.swift
+//  Video List
+//
+//  Created by Peter Loux on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Video_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
