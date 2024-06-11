0f2b3bd86576d662b780415c5ab68ecc154476b0
Ploux <peter@humo.golf>
2024-06-11 13:34:26 -0500

    add data

--- Video List/VideoListView.swift
+++ Video List/VideoListView.swift
@@ -8,16 +8,34 @@
 import SwiftUI
 
 struct VideoListView: View {
+    var videos: [Video] = VideoList.topTen
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        NavigationView {
+            List(videos, id: \.id) { video in
+                HStack {
+                    Image("average-dev")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 70)
+                        .cornerRadius(4)
+                    VStack (alignment: .leading, spacing: 5) {
+                        Text("I'm an Average Developer")
+                            .fontWeight(.semibold)
+                            .lineLimit(2)
+                            .minimumScaleFactor(0.5)
+                        Text("January 1, 2021")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            } // end List
+            .navigationTitle("Top Ten")
+        
+        } // end NavigationView
+        
+    } // end of body
+    
+} // end VideoListView
 
 #Preview {
     VideoListView()
